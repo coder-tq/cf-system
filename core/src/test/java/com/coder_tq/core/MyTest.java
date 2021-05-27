b1131a32c8b41934ce3e2c57c9f92a50c7cb8815
coder_tq <909413805@qq.com>
2021-05-27 22:39:20 +0800

    添加RankController

--- core/src/test/java/com/coder_tq/core/MyTest.java
+++ core/src/test/java/com/coder_tq/core/MyTest.java
@@ -1,13 +1,16 @@
 package com.coder_tq.core;
 
+import cn.coder_tq.core.CoreApplication;
 import cn.coder_tq.core.mapper.CfInfoMapper;
 import cn.coder_tq.core.service.CfInfoService;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+@SpringBootTest(classes = CoreApplication.class)
+@RunWith(SpringRunner.class)
 public class MyTest {
 
     @Autowired
