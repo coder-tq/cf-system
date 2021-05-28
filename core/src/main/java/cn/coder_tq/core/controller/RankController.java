896b6a117eff5d0820b465d8f3e8142251d033aa
AkatoshSR <sqx2211@qq.com>
2021-05-28 20:02:53 +0800

    分页参数

--- core/src/main/java/cn/coder_tq/core/controller/RankController.java
+++ core/src/main/java/cn/coder_tq/core/controller/RankController.java
@@ -1,6 +1,7 @@
 package cn.coder_tq.core.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -15,7 +16,8 @@ public class RankController {
      * 查询群内排名信息
      */
     @RequestMapping("/rank")
-    public void showRank(long groupNumber){
+    public void showRank(long groupNumber,@PathVariable long current,
+                                          @PathVariable long limit){
 
     }
 
