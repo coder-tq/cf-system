c704b9b5570dba9457e13e4376546299e2714413
AkatoshSR <sqx2211@qq.com>
2021-05-28 20:14:06 +0800

    分页参数

--- core/src/main/java/cn/coder_tq/core/service/Impl/CfUserMergeServiceImpl.java
+++ core/src/main/java/cn/coder_tq/core/service/Impl/CfUserMergeServiceImpl.java
@@ -4,6 +4,8 @@ import cn.coder_tq.core.DO.CfUserMerge;
 import cn.coder_tq.core.mapper.CfUserMergeMapper;
 import cn.coder_tq.core.service.CfUserMergeService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
 
+@Service
 public class CfUserMergeServiceImpl extends ServiceImpl<CfUserMergeMapper, CfUserMerge> implements CfUserMergeService {
 }
--- core/src/main/java/cn/coder_tq/core/service/Impl/UserInfoServiceImpl.java
+++ core/src/main/java/cn/coder_tq/core/service/Impl/UserInfoServiceImpl.java
@@ -4,7 +4,9 @@ import cn.coder_tq.core.DO.UserInfo;
 import cn.coder_tq.core.mapper.UserInfoMapper;
 import cn.coder_tq.core.service.UserInfoService;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import org.springframework.stereotype.Service;
 
+@Service
 public class UserInfoServiceImpl extends ServiceImpl<UserInfoMapper, UserInfo> implements UserInfoService {
     @Override
     public boolean checkUser(UserInfo user) {
