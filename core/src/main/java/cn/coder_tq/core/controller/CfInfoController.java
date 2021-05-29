677ffb23dae08b9729b1411c2e51a1f019429c92
coder_tq <909413805@qq.com>
2021-05-29 22:04:52 +0800

    更新逻辑

--- core/src/main/java/cn/coder_tq/core/controller/CfInfoController.java
+++ core/src/main/java/cn/coder_tq/core/controller/CfInfoController.java
@@ -16,4 +16,23 @@ import org.springframework.web.bind.annotation.RestController;
 public class CfInfoController {
     @Autowired
     CfInfoService cfInfoService;
+
+    /**
+     * 更新所有CfInfo，使用多线程实现，每天自动调用。外部调用需要管理员权限。已经逻辑删除的数据不应该进行更新。
+     * @return 更新结果
+     */
+    public Result updateAllCfInfo() {
+        //TODO
+        return Result.ok();
+    }
+
+    /**
+     * 更新一个cfInfo。异步。
+     * @param cfInfo 需要更新的cfInfo。
+     * @return 更新结果。
+     */
+    public Result updateCfInfo(CfInfo cfInfo) {
+        //TODO
+        return Result.ok();
+    }
 }
